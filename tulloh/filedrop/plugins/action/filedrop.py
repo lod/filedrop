9e8fca953dca38e642630298b00c6a8a803dce8c
David Tulloh <git-david@tulloh.id.au>
2025-04-25 18:59:44 +1000

    Add user/group specifying for files/templates - not directories yet

--- tulloh/filedrop/plugins/action/filedrop.py
+++ tulloh/filedrop/plugins/action/filedrop.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import re
 import os
 import typing
 from typing import Any
@@ -31,7 +32,39 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.parsing import dataloader as _dataloader
 
 DOCUMENTATION = r"""
-
+---
+module: filedrop
+short_description: Drop a directory tree of files onto the remote system
+description:
+    - Bob
+    - Smith
+options:
+    owner_re:
+        description:
+        - Set file ownership using file regular expressions.
+        - This is passed a dictionary, the key is a regular expression, the value is the owner name or id.
+        - Any path which matches the regular expression (using re.search()) has the owner set as specified.
+        - Later expressions can overwrite earlier ones, this is also overwritten by owner_exact.
+        - The ansible.builtin.copy rules also apply, numeric owners are user IDs and not specifying an owner means that the current user is used or existing owner if running as root.
+    owner_exact:
+        description:
+        - Set the file ownership using exact full path matches.
+        - This is passed as a dictionary, the key is the file path, the value is the owner name or id.
+        - The ansible.builtin.copy rules also apply, numeric owners are user IDs and not specifying an owner means that the current user is used or existing owner if running as root.
+        - It is recommended that you use this sparingly, using owner_re will typically result in cleaner code. Extensive use also defeats the flexibility that this module is designed to provide.
+    group_re:
+        description:
+        - Set file group ownership using file regular expressions.
+        - This is passed a dictionary, the key is a regular expression, the value is the group name or id.
+        - Any path which matches the regular expression (using re.search()) has the group set as specified.
+        - Later expressions can overwrite earlier ones, this is also overwritten by group_exact.
+        - The ansible.builtin.copy rules also apply, numeric groups are user IDs and not specifying a group means that the current user default group is used or existing group if running as root.
+    group_exact:
+        description:
+        - Set the file group ownership using exact full path matches.
+        - This is passed as a dictionary, the key is the file path, the value is the file group name or id.
+        - The ansible.builtin.copy rules also apply, numeric groups are user IDs and not specifying a groups means that the current user default group is used or existing group if running as root.
+        - It is recommended that you use this sparingly, using group_re will typically result in cleaner code. Extensive use also defeats the flexibility that this module is designed to provide.
 """
 
 
@@ -55,8 +88,14 @@ class ActionModule(ActionBase):
         ret = super(ActionModule, self).run(tmp, task_vars)
         ret["tree"] = {}
 
+        self._task_vars = task_vars
+
         # source = self._task.args.get('src', None)
         source = "files"
+        owner_exact = self._task.args.get('owner_exact', {})
+        group_exact = self._task.args.get('group_exact', {})
+        owner_re = { re.compile(k):v for k,v in self._task.args.get('owner_re', {}).items()}
+        group_re = { re.compile(k):v for k,v in self._task.args.get('group_re', {}).items()}
 
         try:
             source_path = self._find_needle("templates", source)
@@ -90,11 +129,15 @@ class ActionModule(ActionBase):
             for filename in files:
                 p = base / filename
                 if p.suffix == ".j2":
+                    owner = self.search_matches(p.with_suffix(""), owner_re, owner_exact)
+                    group = self.search_matches(p.with_suffix(""), group_re, group_exact)
                     ret["tree"][str(p)] = self.process_template(
-                        p, source_path, template_searchpath
+                        p, source_path, template_searchpath, owner, group
                     )
                 else:
-                    ret["tree"][str(p)] = self.process_file(p, source_path)
+                    owner = self.search_matches(p, owner_re, owner_exact)
+                    group = self.search_matches(p, group_re, group_exact)
+                    ret["tree"][str(p)] = self.process_file(p, source_path, owner, group)
                 # TODO: Add .keep and _keep files, make it a customisable parameter
                 # TODO: Figure out how symlinks work, especially symlinks out of the tree
 
@@ -113,7 +156,7 @@ class ActionModule(ActionBase):
             module_name="ansible.legacy.file",
             # module_args mode: for permissions, and owner and group
             module_args={"path": str(path), "state": "directory"},
-            task_vars={"ansible_facts": {}},
+            task_vars=self._task_vars,
         )
 
         return {
@@ -122,7 +165,7 @@ class ActionModule(ActionBase):
         }
 
     def process_template(
-        self, path: Path, local_root: Path, searchpath: list[str]
+        self, path: Path, local_root: Path, searchpath: list[str], owner: str|None, group: str|None
     ) -> dict[str, str | int | bool]:
         Display().vvv(f"Processing template {local_root} {path}")
 
@@ -150,22 +193,22 @@ class ActionModule(ActionBase):
             result_file = local_tempdir / path.name
             result_file.write_text(resultant)
 
-            copy_return = self.copy_action(result_file, path.with_suffix(""))
+            copy_return = self.copy_action(result_file, path.with_suffix(""), owner, group)
         finally:
             Path(result_file).unlink()
             local_tempdir.rmdir()
 
         return copy_return
 
-    def process_file(self, path: Path, local_root: Path) -> dict[str, str | int | bool]:
+    def process_file(self, path: Path, local_root: Path, owner: str|None, group: str|None) -> dict[str, str | int | bool]:
         Display().vvv(f"Processing file {local_root} {path}")
 
         fullpath = local_root / path.relative_to("/")
 
-        return self.copy_action(fullpath, path)
+        return self.copy_action(fullpath, path, owner, group)
 
     def copy_action(
-        self, source_file: Path, dest_file: Path
+            self, source_file: Path, dest_file: Path, owner: str|None = None, group: str|None = None
     ) -> dict[str, str | int | bool]:
         # call with ansible.legacy prefix to eliminate collisions with collections while still allowing local override
         copy_task = Task()
@@ -175,6 +218,11 @@ class ActionModule(ActionBase):
             "dest": str(dest_file),
             "follow": True,
         }
+        if owner is not None:
+            copy_task.args["owner"] = owner
+        if group is not None:
+            copy_task.args["group"] = group
+
         # TODO: Pass through checkmode?
         # TODO: Pass through async?
         copy_action = self._shared_loader_obj.action_loader.get(
@@ -186,7 +234,7 @@ class ActionModule(ActionBase):
             templar=self._templar,
             shared_loader_obj=self._shared_loader_obj,
         )
-        copy_return = copy_action.run(task_vars={"ansible_facts": {}})
+        copy_return = copy_action.run(self._task_vars)
 
         # Trim back result to the bits we actually care about
         return {
@@ -203,6 +251,20 @@ class ActionModule(ActionBase):
                 "checksum",
             )
         }
+
+    @staticmethod
+    def search_matches(test_path: Path, re_matches: dict[re.Pattern, str], exact_matches: dict[str,str]) -> str|None:
+        # exact matches overwrite regex matches
+        # later matches overwrite earlier matches (python keeps ordered dicts now)
+
+        match = None  # default
+        for reg, potential in re_matches.items():
+            if reg.search(str(test_path)) is not None:
+                match = potential
+        for exact, potential in exact_matches.items():
+            if exact == str(test_path):
+                match = potential
+        return match
 
     def _find_needle(self, dirname: str, needle: str) -> Path:
         """
